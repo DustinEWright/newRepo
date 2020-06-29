282380d92071bc2bc4cdd6e7056733373f77dbd9
Dustin Wright <nzeroxpd@gmail.com>
2020-06-29 15:00:49 -0500

    Added starter project files.

---
+++ newRepo/src/main/java/com/fred/newRepo/NewRepoApplication.java
@@ -0,0 +1,13 @@
+package com.fred.newRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewRepoApplication.class, args);
+	}
+
+}
